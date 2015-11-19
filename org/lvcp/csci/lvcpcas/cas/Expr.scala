8a134fccdd78a87e8b534b8bb97a75d485bb7967
Dimitriye Danilović <dimitriye@danilovic.net>
2015-11-18 18:43:38 -0800

    Add variable substitution

--- org/lvcp/csci/lvcpcas/cas/Expr.scala
+++ org/lvcp/csci/lvcpcas/cas/Expr.scala
@@ -1,10 +1,30 @@
 package org.lvcp.csci.lvcpcas.cas
 
-abstract class Expr
+abstract class Expr {
+	def substitute(substitutions: (Var, Expr)*): Expr
+}
 
-case class Sum(addends: Expr*) extends Expr
-case class Product(factors: Expr*) extends Expr
-case class Power(base: Expr, exponent: Expr) extends Expr
-case class ScalarConst(value: BigInt) extends Expr
-case class Var(name: String) extends Expr
+case class Sum(addends: Expr*) extends Expr {
+	override def substitute(substitutions: (Var, Expr)*) =
+		Sum(addends.map{ _.substitute(substitutions:_*) })
+}
+
+case class Product(factors: Expr*) extends Expr {
+	override def substitute(substitutions: (Var, Expr)*) =
+		Product(factors.map{ _.substitute(substitutions:_*) })
+}
+
+case class Power(base: Expr, exponent: Expr) extends Expr {
+	override def substitute(substitutions: (Var, Expr)*) =
+		Power(base.substitute(substitutions:_*), exponent.substitute(substitutions:_*))
+}
+
+case class ScalarConst(value: BigInt) extends Expr {
+	override def substitute(substitutions: (Var, Expr)*) = this
+}
+
+case class Var(name: String) extends Expr {
+	override def substitute(substitutions: (Var, Expr)*) =
+		substitutions.toMap.getOrElse(this, this)
+}
 
